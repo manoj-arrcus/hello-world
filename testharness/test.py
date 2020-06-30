6db4fd2e4149dd34f815f6a84c2ea7bf80d7af7c
Manoj Gupta <manoj@arrcus.com>
2020-06-30 17:09:57 +0530

    TH-Build+TH-Regression

--- testharness/test.py
+++ testharness/test.py
@@ -1,14 +1,13 @@
 import sys 
 #POC file to route actions based on git commit comments  
-if sys.argv == 1:
-	return
+if sys.argv > 1:
 
-if sys.argv[1].find('TH-Build') >= 0:
-	printf("Build c code and execute")
+	if sys.argv[1].find('TH-Build') >= 0:
+		printf("Build c code and execute")
 
-if sys.argv[1].find('TH-Upgrade') >= 0:
-	printf("Upgrade Switches listed in second argument")
+	if sys.argv[1].find('TH-Upgrade') >= 0:
+		printf("Upgrade Switches listed in second argument")
 
-if sys.argv[1].find('TH-Regression') >= 0:
-	printf("Trigger regression over the testbed")
+	if sys.argv[1].find('TH-Regression') >= 0:
+		printf("Trigger regression over the testbed")
 
