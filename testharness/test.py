7191d6cae8bb62b6bf99ec1ecd68783422914645
Manoj Gupta <manoj@arrcus.com>
2020-06-30 17:57:19 +0530

    TH-Build+TH-Upgrade+TH-Regression

--- testharness/test.py
+++ testharness/test.py
@@ -1,6 +1,9 @@
 import sys 
 import os 
 
+bin_url = "http://10.9.195.14:7777/2020_06_25/ArcOS_v4.1.1.EFT6/ONL/ARCOS-ArcOS_v4.1.1.EFT6_ONL-OS_2020-06-26.0221-7f9bfa0_AMD64_INSTALLED_INSTALLER"
+host_ip = "10.9.9.26"
+
 #POC file to route actions based on git commit comments  
 if sys.argv > 1:
 
@@ -11,6 +14,17 @@ if sys.argv > 1:
 		print("\nTest Harness:Build c code and execute")
 
 	if sys.argv[1].find('TH-Upgrade') >= 0:
+		fp = open("upgrade_onl_template.xml","r")
+		fw = open("upgrade_onl.xml","w")
+		data = fp.readlines()
+		for line in data: 
+			if line.find("package-name") >= 0:
+				fw.write("<package-name>{}</package-name>".format(bin_url))
+			else
+				fw.write(line)
+		fp.close()
+		fw.close()
+		os.system("./netconf-console --host {} --port 830 -u root -p arrcus --rpc upgrade_onl.xml".format(host_ip))
 		print("Test Harness:Upgrade Switches listed in second argument")
 
 	if sys.argv[1].find('TH-Regression') >= 0:
