9fc94cad3d712355cab4427c759d620ad7d3521a
Manoj Gupta <manoj@arrcus.com>
2020-06-30 17:12:00 +0530

    TH-Build+TH-Regression

--- testharness/test.py
+++ testharness/test.py
@@ -3,11 +3,11 @@ import sys
 if sys.argv > 1:
 
 	if sys.argv[1].find('TH-Build') >= 0:
-		printf("Build c code and execute")
+		print("Build c code and execute")
 
 	if sys.argv[1].find('TH-Upgrade') >= 0:
-		printf("Upgrade Switches listed in second argument")
+		print("Upgrade Switches listed in second argument")
 
 	if sys.argv[1].find('TH-Regression') >= 0:
-		printf("Trigger regression over the testbed")
+		print("Trigger regression over the testbed")
 
