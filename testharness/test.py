1308027c2c3c69ea578154c4e8275bb75fd4779e
Manoj Gupta <manoj@arrcus.com>
2020-06-30 17:28:28 +0530

    TH-Build+TH-Regression Manoj Gupta Testing Need to check multiple lines comments

--- testharness/test.py
+++ testharness/test.py
@@ -1,9 +1,14 @@
 import sys 
+import os 
+
 #POC file to route actions based on git commit comments  
 if sys.argv > 1:
 
 	if sys.argv[1].find('TH-Build') >= 0:
 		print("Test Harness:Build c code and execute")
+		os.system("gcc helloworld.c")
+		os.system("./a.out")
+		os.system("rm a.out")
 
 	if sys.argv[1].find('TH-Upgrade') >= 0:
 		print("Test Harness:Upgrade Switches listed in second argument")
